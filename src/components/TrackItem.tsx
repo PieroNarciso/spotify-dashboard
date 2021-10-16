8ab6c9c947b90d4643a1047f47b711f5181f6cd2
Piero Narciso <pieronarciso988@gmail.com>
2021-10-16 12:36:25 -0500

    Show controls on hover (TrackItem)
    * Show toggle button (save track)

--- src/components/TrackItem.tsx
+++ src/components/TrackItem.tsx
@@ -1,6 +1,8 @@
-import { useAppSelector, useAudio } from '@/hooks';
+import { useAppDispatch, useAppSelector, useAudio } from '@/hooks';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { Track } from '@/interfaces';
 import React from 'react';
+import { removeSavedTrack, saveTrack } from '@/store/spotify.thunks';
 
 interface TrackItemProps {
   track: Track;
@@ -9,16 +11,26 @@ interface TrackItemProps {
 
 const TrackItem: React.FC<TrackItemProps> = ({ className, ...props }) => {
   const volume = useAppSelector((state) => state.spotify.volume);
+  const dispatch = useAppDispatch();
   const percentVolumen = volume / 100;
   const { play, pause } = useAudio(
     props.track.preview_url || '',
     percentVolumen
   );
+
+  const toggleSaveTrack = async () => {
+    if (props.track.saved) {
+      await dispatch(removeSavedTrack(props.track.id)).unwrap();
+    } else {
+      await dispatch(saveTrack(props.track.id)).unwrap();
+    }
+  };
+
   return (
     <div
       onMouseEnter={play}
       onMouseLeave={pause}
-      className={`focus:shadow-lg hover:shadow-lg ${
+      className={`focus:shadow-lg hover:shadow-lg group relative ${
         className ? className : ''
       }`}
     >
@@ -27,6 +39,23 @@ const TrackItem: React.FC<TrackItemProps> = ({ className, ...props }) => {
         src={props.track.album?.images[0].url}
         alt={props.track.name}
       />
+      <div className="bg-neutral-focus absolute opacity-90 top-0 left-0 w-full h-full hidden group-hover:flex flex-col justify-between p-2">
+        <span className="text-accent font-semibold text-center self-center">
+          {props.track.name}
+        </span>
+        <div className="flex justify-end">
+          <button
+            className="btn btn-circle btn-sm font-semibold"
+            onClick={toggleSaveTrack}
+          >
+            {props.track.saved ? (
+              <AiFillHeart className="text-secondary-focus w-6 h-6" />
+            ) : (
+              <AiOutlineHeart className="text-secondary-focus w-6 h-6" />
+            )}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
